3cd1d0953ef23e8610d9f65d01be02fb09c13c33
Luke Worley <luke.worley@colorado.edu>
2018-05-14 21:32:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  const tree = document.getElementById("nested").querySelector(".target")
+  return tree
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll(".ranked-list")
+
+  var i
+  for (i=0;i<list.length;i++){
+    var current=list[i]
+    var j
+    for(j=0;j<current.children.length;j++){
+      current.children[j].innerHTML = parseInt(current.children[j].innerHTML) + n
+    }
+  }
+  
+}
+function deepestChild(){
+    const tree = document.querySelectorAll("#grand-node")[0]
+    var child=tree.children[0]
+    if (child == null){
+      return tree
+    }
+  
+    while(child.children[0] != null){
+      var child=child.children[0]
+    }
+    
+    return child
+}
