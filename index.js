e09a86290fba7ac25818955d6adb59b771d065a8
Luke Worley <luke.worley@colorado.edu>
2018-05-14 20:54:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function nestedTarget(){
 function deepestChild(){
     const tree = document.querySelectorAll("#grand-node")
     var child=tree.children[0]
-
+    return child
+  /*
     while(child.children[0] != null){
       console.log(children[0])
       var child=tree.children[0]
     }
-    return child
+    return child*/
 }
