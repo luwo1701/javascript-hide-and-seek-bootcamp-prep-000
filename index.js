df2204c95a5d5c63fe3a7fa7022a8eb4a0f45b6f
Luke Worley <luke.worley@colorado.edu>
2018-05-14 20:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function getFirstSelector(selector){
 function nestedTarget(){
   const tree = document.querySelectorAll("#nested")
   var i=0
-  for (i=0;i<tree.)
+  for (i=0;i<tree.length;i++){
+    
+  }
   console.log(tree[0])
   return tree
 }
